aa4652ecd48b5357b37d9aa3122d8ab95c459a68
凌莞 <i@gao4.pw>
2021-11-27 12:07:49 +0800

    fix build only from aur

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,10 @@
 
 pkgname=$1
 
+yay -Sy
+
 useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
-sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"
+sudo --set-home -u builder yay -Sa --noconfirm --builddir=./ "$pkgname"
