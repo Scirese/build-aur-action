75f51d170084e80fef3424cbc9cb63328163a114
凌莞 <i@gao4.pw>
2022-01-07 20:56:51 +0800

    做成通用架构的

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,20 @@ yay -Syu
 
 useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+if [[ $ARCH != "x86_64" ]]; then
+  git clone https://aur.archlinux.org/$pkgname.git
+  cd $pkgname
+  source PKGBUILD
+fi
+
 chmod -R a+rw .
 
-sudo --set-home -u builder yay -Sa --noconfirm --useask --builddir=./ "$pkgname"
-
+if [[ $ARCH == "x86_64" ]]; then
+  sudo --set-home -u builder yay -Sa --noconfirm --useask --builddir=./ "$pkgname"
+else
+  sudo --set-home -u builder yay -S --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}"
+  sudo --set-home -u builder CARCH=$ARCH makepkg -sfA
+fi
 cd $pkgname
 echo ::set-output name=filelist::$(sudo --set-home -u builder makepkg --packagelist | xargs)
