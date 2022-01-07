0802f04a8d357d480fcc8d2f3f1c5efbbd842ee5
凌莞~(=^▽^=) <hujl_123@yeah.net>
2022-01-07 20:23:26 +0800

    修复构建冲突的包时候的问题

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
-sudo --set-home -u builder yay -Sa --noconfirm --builddir=./ "$pkgname"
+sudo --set-home -u builder yay -Sa --noconfirm --useask --builddir=./ "$pkgname"
 
 cd $pkgname
 echo ::set-output name=filelist::$(sudo --set-home -u builder makepkg --packagelist | xargs)
