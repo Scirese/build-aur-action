21b41306a80071aec744450ad927d3c0d5f3fc64
凌莞~(=^▽^=) <hujl_123@yeah.net>
2022-01-07 21:05:58 +0800

    不是 x86_64 的不用再次 cd 了

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,9 +17,9 @@ chmod -R a+rw .
 
 if [[ $ARCH == "x86_64" ]]; then
   sudo --set-home -u builder yay -Sa --noconfirm --useask --builddir=./ "$pkgname"
+  cd $pkgname
 else
   sudo --set-home -u builder yay -S --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}"
   sudo --set-home -u builder CARCH=$ARCH makepkg -sfA
 fi
-cd $pkgname
 echo ::set-output name=filelist::$(sudo --set-home -u builder makepkg --packagelist | xargs)
