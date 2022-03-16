617341fd29475d728e02e7a965d27cacb824c79a
凌莞~(=^▽^=) <hujl_123@yeah.net>
2022-03-16 17:08:28 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,23 +9,11 @@ echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo 'PACKAGER="Clansty <i@gao4.pw>"
 COMPRESSZST=(zstd -19 -c -z -q --threads=0 -)' > /home/builder/.makepkg.conf
 
-if [[ $ARCH == "x86_64" ]]; then
-  echo qwq
-elif [[ $ARCH == "i686" ]]; then
-  export CFLAGS+=" -m32"
-  export CXXFLAGS+=" -m32"
-  export LDFLAGS+=" -m32"
-  export PKG_CONFIG_PATH='/usr/lib32/pkgconfig'
-  rm /etc/pacman.conf
-  mv /etc/pacman32.conf /etc/pacman.conf
-  pacman -Syy
-else
-  if [[ $pkgname != ./* ]];then
-    git clone https://aur.archlinux.org/$pkgname.git
-  fi # 否则为本地包
-  cd $pkgname
-  source PKGBUILD
-fi
+if [[ $pkgname != ./* ]];then
+  git clone https://aur.archlinux.org/$pkgname.git
+fi # 否则为本地包
+cd $pkgname
+source PKGBUILD
 
 chmod -R a+rw .
 
